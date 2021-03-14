b44effb5fc4c97298f7694e1723b249aa31a16d1
Hanna Skeppe <hannaskeppe@gmail.com>
2021-03-14 22:42:25 +0100

    Logic to delete a note and change color in place.

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ class App {
     this.text = '' // store note texts
     this.id = '' // store id:s of the notes
 
-    this.$placeholder = document.querySelector('#placeholder');
-    this.$form = document.querySelector('#form')// $ before 'this' because quering for an html-element (and not data)
-    this.$notes = document.querySelector('#notes');
+    this.$placeholder = document.querySelector('#placeholder')
+    this.$form = document.querySelector('#form') // $ before 'this' because quering for an html-element (and not data)
+    this.$notes = document.querySelector('#notes')
     this.$noteTitle = document.querySelector('#note-title')
     this.$noteText = document.querySelector('#note-text')
     this.$formButtons = document.querySelector('#form-buttons')
@@ -16,6 +16,7 @@ class App {
     this.$modalTitle = document.querySelector(".modal-title")
     this.$modalText = document.querySelector(".modal-text")
     this.$modalCloseButton = document.querySelector('.modal-close-button')
+    this.$colorTooltip = document.querySelector('#color-tooltip')
 
     this.addEventListeners()
   }
@@ -25,6 +26,30 @@ class App {
       this.handleFormClick(event)
       this.selectNote(event) // to select a specific note to edit
       this.openModal(event) // --> this has to be placed after selectNote in order for the modal to be populated accordingly with the text & title values.
+      this.deleteNote(event)
+    })
+
+    document.body.addEventListener('mouseover', event => { // to display tooltip when hovering the palette on the note
+      this.openTooltip(event) // recieves the event
+    })
+
+    document.body.addEventListener('mouseout', event => { // to close tooltip on mouseout
+      this.closeTooltip(event) 
+    })
+
+    this.$colorTooltip.addEventListener('mouseover', function() {
+      this.style.display = 'flex';  
+    })
+    
+    this.$colorTooltip.addEventListener('mouseout', function() {
+      this.style.display = 'none'; 
+    })
+
+    this.$colorTooltip.addEventListener('click', event => {
+      const color = event.target.dataset.color
+      if (color) {
+        this.editNoteColor(color)
+      }
     })
 
     this.$form.addEventListener('submit', event => {
@@ -73,11 +98,13 @@ class App {
     this.$form.classList.remove('form-open')
     this.$noteTitle.style.display = 'none'
     this.$formButtons.style.display = 'none'
-    this.$noteTitle.value = '';
-    this.$noteText.value = '';
+    this.$noteTitle.value = ''
+    this.$noteText.value = ''
   }
 
   openModal(event) {
+    if (event.target.matches('.toolbar-delete')) return // don't open modal if clicking on the delete or color icon.
+    if (event.target.matches('.toolbar-color')) return
     if (event.target.closest('.note')) {
       this.$modal.classList.toggle('open-modal')
       this.$modalTitle.value = this.title // populate the title & text in the modal with existing values
@@ -87,7 +114,24 @@ class App {
 
   closeModal(event) { // Close modal and edit the content in the note on close
     this.editNote()
-    this.$modal.classList.toggle('open-modal');  
+    this.$modal.classList.toggle('open-modal') 
+  }
+
+  openTooltip(event) {
+    if (!event.target.matches('.toolbar-color')) return
+    this.id = event.target.dataset.id
+    const noteCoordinates = event.target.getBoundingClientRect() // get the coordinates of where the user is hovering.
+    const horizontal = noteCoordinates.left
+    const vertical = window.scrollY - 20
+    // const horizontal = noteCoords.left + window.scrollX; // Doesn't get optimal positioning using these. How to fix it?
+    // const vertical = noteCoords.top + window.scrollY;
+    this.$colorTooltip.style.transform = `translate(${horizontal}px, ${vertical}px)` 
+    this.$colorTooltip.style.display = 'flex'
+  }
+
+  closeTooltip(event) {
+    if (!event.target.matches('.toolbar-color')) return
+    this.$colorTooltip.style.display = 'none'
   }
 
   addNote({ title, text }) {
@@ -111,6 +155,13 @@ class App {
     this.displayNotes()
   }
 
+  editNoteColor(color) {
+    this.notes = this.notes.map(note =>
+      note.id === Number(this.id) ? { ...note, color } : note
+    );
+    this.displayNotes();
+  }
+
   selectNote(event) {
     const $selectedNote = event.target.closest('.note')
     // console.log($selectedNote.children)
@@ -119,6 +170,14 @@ class App {
     this.title = $noteTitle.innerText // get the content of the elements set them equal to the values of text / title
     this.text = $noteText.innerHTML
     this.id = $selectedNote.dataset.id // get the data-id from the note (note.id) & update it
+  }
+
+  deleteNote(event) {
+    event.stopPropagation() // to prevent 'bubbling', not to have the modal open up when clicking delete
+    if (!event.target.matches('.toolbar-delete')) return
+    const id = event.target.dataset.id // local id variable
+    this.notes = this.notes.filter(note => note.id !== Number(id)) // filter out all notes except the one to delete
+    this.displayNotes() 
   }
 
   displayNotes() {
@@ -131,13 +190,14 @@ class App {
         <div class="note-text">${note.text}</div>
         <div class="toolbar-container">
           <div class="toolbar">
-            <div class="toolbar-color"></div>
-            <svg 
-              class="toolbar-delete" 
+            <div class="toolbar-color" data-id=${note.id}></div>
+            <svg
+              class="toolbar-delete"
+              data-id=${note.id} 
               xmlns="http://www.w3.org/2000/svg" 
-              width="24" 
-              height="24" 
-              viewBox="0 0 24 24">
+              width="20" 
+              height="20" 
+              viewBox="0 0 20 20">
               <path d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13z"></path>
               <path d="M9 8h2v9H9zm4 0h2v9h-2z"></path>
             </svg>
